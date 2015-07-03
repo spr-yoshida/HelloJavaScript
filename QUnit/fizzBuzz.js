1e30dde570772a19115aaeeb6671441ecb2b238c
spry <yoshida@spr.jp>
2015-07-04 02:18:31 +0900

    Adjustment indent

--- QUnit/fizzBuzz.js
+++ QUnit/fizzBuzz.js
@@ -1,16 +1,16 @@
 function fizzBuzz(n) {
-	if (n <= 0) {
-		return "Out of range";
-	}
-	if ((n % 3 == 0) && (n % 5 == 0)) {
-		return "FizzBuzz";
-	}
-	if (n % 3 == 0) {
-		return "Fizz";
-	}
-	if (n % 5 == 0) {
-		return "Buzz";
-	}
+    if (n <= 0) {
+        return "Out of range";
+    }
+    if ((n % 3 == 0) && (n % 5 == 0)) {
+        return "FizzBuzz";
+    }
+    if (n % 3 == 0) {
+        return "Fizz";
+    }
+    if (n % 5 == 0) {
+        return "Buzz";
+    }
 
-	return n;
+    return n;
 }
--- QUnit/fizzBuzzTest.js
+++ QUnit/fizzBuzzTest.js
@@ -1,35 +1,35 @@
 QUnit.test( "Fizz", function( assert ) {
-	var expected = "Fizz"
-	var actual = fizzBuzz(3);
-	assert.equal(expected, actual, "Passed!");
+    var expected = "Fizz"
+    var actual = fizzBuzz(3);
+    assert.equal(expected, actual, "Passed!");
 });
 
 QUnit.test( "Buzz", function( assert ) {
-	var expected = "Buzz"
-	var actual = fizzBuzz(5);
-	assert.equal(expected, actual, "Passed!");
+    var expected = "Buzz"
+    var actual = fizzBuzz(5);
+    assert.equal(expected, actual, "Passed!");
 });
 
 QUnit.test( "Fizz Buzz", function( assert ) {
-	var expected = "FizzBuzz"
-	var actual = fizzBuzz(15);
-	assert.equal(expected, actual, "Passed!");
+    var expected = "FizzBuzz"
+    var actual = fizzBuzz(15);
+    assert.equal(expected, actual, "Passed!");
 });
 
 QUnit.test( "Other", function( assert ) {
-	var expected = "1"
-	var actual = fizzBuzz(1);
-	assert.equal(expected, actual, "Passed!");
+    var expected = "1"
+    var actual = fizzBuzz(1);
+    assert.equal(expected, actual, "Passed!");
 });
 
 QUnit.test( "Out of Range - Zero", function( assert ) {
-	var expected = "Out of range"
-	var actual = fizzBuzz(0);
-	assert.equal(expected, actual, "Passed!");
+    var expected = "Out of range"
+    var actual = fizzBuzz(0);
+    assert.equal(expected, actual, "Passed!");
 });
 
 QUnit.test( "Out of Range - Minus", function( assert ) {
-	var expected = "Out of range"
-	var actual = fizzBuzz(-3);
-	assert.equal(expected, actual, "Passed!");
+    var expected = "Out of range"
+    var actual = fizzBuzz(-3);
+    assert.equal(expected, actual, "Passed!");
 });
